df6ba55486fbb338ccc431431d78c8bdb11e9c4b
Robson Formigão <robson.formigao@scaffoldeducation.com.br>
2021-12-28 22:41:28 -0300

    feat(app): finish first module

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -21,7 +27,7 @@ export function RepositoryList() {
           repositories.map(repository => {
             return (
               <RepositoryItem
-                key={repository.id}
+                key={repository.name}
                 repository={repository}
               />
             )
